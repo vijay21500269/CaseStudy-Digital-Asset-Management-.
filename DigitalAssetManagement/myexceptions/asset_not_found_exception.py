f1becbc7e9019a8fc47b9987135368c6190a4cc0
Vijay <94381788+vijay21500269@users.noreply.github.com>
2025-04-17 21:12:07 +0530

    Delete DigitalAssetManagement directory

--- DigitalAssetManagement/myexceptions/asset_not_found_exception.py
+++
@@ -1,3 +0,0 @@
-class AssetNotFoundException(Exception):
-    def __init__(self, message="Asset not found."):
-        super().__init__(message)
--- DigitalAssetManagement/myexceptions/asset_not_maintain_exception.py
+++
@@ -1,3 +0,0 @@
-class AssetNotMaintainException(Exception):
-    def __init__(self, message="Asset has not been maintained for more than 2 years."):
-        super().__init__(message)
